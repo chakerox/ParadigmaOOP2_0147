a6f5b810f0f6ca378ba86d9efcc665e06f58be0b
chakerox <chakerox@gmail.com>
2026-05-19 20:11:32 +0700

    membuat class remote lampu dan membuat array string di private dan membuat void set dan fungsi get

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -1,3 +1,14 @@
 #include<iostream>
 using namespace std;
 
+class remotelampu{
+    private:
+        string saklarno[10];
+    public:
+        void setSaklarno(int i,string value){
+            saklarno[i] = value;
+        }
+        string getSaklarno(int i){
+            return saklarno[i];
+        }
+};
