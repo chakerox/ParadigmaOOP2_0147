29127cbf4bd20d839e2cb815b72191a295d147ad
chakerox <chakerox@gmail.com>
2026-05-19 20:35:55 +0700

    menambahkan library

---
+++ abstraksi.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+class AbsraksiKlas{
+    private:
+        string x,y;
+    public:
+
+        // method untuk mengisi nilai
+        // private member
+        void setXY(string a, string b){
+            x = a;
+            y = b;
+        }
+        //menampilkan nilai private member
+        void display(){
+            cout << "nilai x = " << x << endl;
+            cout << "nilai y = " << y << endl;
+        }
+}
